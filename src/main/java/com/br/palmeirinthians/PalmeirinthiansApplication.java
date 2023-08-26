ec9cbf32ec1e5abe6ebcc3d0058cd875272873fe
Gustavo <guolmos06@gmail.com>
2023-08-26 18:48:43 -0300

    blablabla

---
+++ src/main/java/com/br/palmeirinthians/PalmeirinthiansApplication.java
@@ -0,0 +1,13 @@
+package com.br.palmeirinthians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmeirinthiansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmeirinthiansApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/palmeirinthians/services/TimesService.java
@@ -0,0 +1,3 @@
+package com.br.palmeirinthians.services;
+
+public interface TimesService { }
